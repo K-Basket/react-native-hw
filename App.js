7f85c6f60c4a1fbd18f101a0b5c83ea4d5940b83
K-Basket <koshykkv@gmail.com>
2023-07-22 18:33:58 +0300

    added fonts

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import { RegistrationScreen } from "./Screens/RegistrationScreen";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import { RegistrationScreen } from './Screens/RegistrationScreen';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    RobotoBold: require('./assets/fonts/Roboto-Bold-700.ttf'),
+    RobotoRegular: require('./assets/fonts/Roboto-Regular-400.ttf'),
+    RobotoMedium: require('./assets/fonts/Roboto-Medium-500.ttf'),
+  });
+
+  if (!fontsLoaded) null; // сначала загрузка шрифтов, потом все остальное
+
   return (
     <View style={styles.container}>
       <RegistrationScreen />
@@ -14,7 +23,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#ffeafeff",
+    backgroundColor: '#ffeafeff',
     // alignItems: "center",
     // justifyContent: "center",
   },
--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -9,27 +9,77 @@ import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
-} from "react-native";
-import logoBG from "../assets/img/photo-bg.jpg";
+} from 'react-native';
+import logoBG from '../assets/img/photo-bg.jpg';
+import { useState } from 'react';
+
+const initialState = {
+  login: '',
+  email: '',
+  password: '',
+};
 
 export function RegistrationScreen() {
-  console.log(Platform.OS);
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [dataInput, setDataInput] = useState(initialState);
+
+  function onShowKeyboard() {
+    setIsShowKeyboard(true);
+  }
+
+  function onHideKeyboard() {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+  }
+
+  function onSubmit() {
+    if (!dataInput.login || !dataInput.email || !dataInput.password)
+      return console.warn('Please fill in all fields!');
+
+    onHideKeyboard();
+    console.log('state :>> ', dataInput);
+    setDataInput(initialState);
+  }
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={onHideKeyboard}>
         <ImageBackground style={styles.imageBG} source={logoBG}>
           <KeyboardAvoidingView
-            behavior={Platform.OS == "ios" ? "padding" : "height"}
+            behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
           >
-            <View style={styles.form}>
+            <View
+              style={{
+                ...styles.form,
+                marginBottom: isShowKeyboard ? 50 : 0,
+              }}
+            >
+              <View>
+                <Text style={styles.titleForm}>Реєстрація</Text>
+              </View>
               <View>
                 <Text style={styles.inputTitle}>Login</Text>
-                <TextInput style={styles.input} textAlign="center" />
+                <TextInput
+                  style={styles.input}
+                  textAlign="center"
+                  onFocus={onShowKeyboard}
+                  value={dataInput.login} // передаем данные из state
+                  onChangeText={value =>
+                    setDataInput(prev => ({ ...prev, login: value }))
+                  } // записываем данные в state
+                />
               </View>
               <View style={{ marginTop: 16 }}>
                 <Text style={styles.inputTitle}>E-Mail</Text>
-                <TextInput style={styles.input} textAlign="center" />
+                <TextInput
+                  style={styles.input}
+                  textAlign="center"
+                  onFocus={onShowKeyboard}
+                  value={dataInput.email}
+                  onChangeText={value =>
+                    setDataInput(prev => ({ ...prev, email: value }))
+                  }
+                />
               </View>
               <View style={{ marginTop: 16 }}>
                 <Text style={styles.inputTitle}>Password</Text>
@@ -37,32 +87,41 @@ export function RegistrationScreen() {
                   style={styles.input}
                   textAlign="center"
                   secureTextEntry={true}
+                  onFocus={onShowKeyboard}
+                  value={dataInput.password}
+                  onChangeText={value =>
+                    setDataInput(prev => ({ ...prev, password: value }))
+                  }
                 />
               </View>
-              <TouchableOpacity style={styles.btn} activeOpacity={0.8}>
+              <TouchableOpacity
+                style={styles.btn}
+                activeOpacity={0.8}
+                onPress={onSubmit}
+              >
                 <Text style={styles.btnTitle}>Зареєструватися</Text>
               </TouchableOpacity>
             </View>
           </KeyboardAvoidingView>
         </ImageBackground>
-      </View>
-    </TouchableWithoutFeedback>
+      </TouchableWithoutFeedback>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
   },
   imageBG: {
     flex: 1,
-    resizeMode: "cover",
+    resizeMode: 'cover',
     // alignItems: "center",
-    justifyContent: "center",
+    justifyContent: 'center',
   },
   title: {
-    color: "#fff",
+    color: '#fff',
     // color: "#212121",
     fontSize: 20,
   },
@@ -70,29 +129,37 @@ const styles = StyleSheet.create({
   form: {
     marginHorizontal: 16,
   },
+  titleForm: {
+    // fontFamily: 'RobotoBold',
+    fontSize: 30,
+    fontWeight: 500,
+    color: '#fff',
+    // color: "#212121",
+    textAlign: 'center',
+  },
   inputTitle: {
-    color: "#fff",
+    color: '#fff',
     marginBottom: 5,
   },
   input: {
     borderWidth: 1,
-    borderColor: "#E8E8E8",
+    borderColor: '#E8E8E8',
     height: 50,
     borderRadius: 6,
-    color: "#fff",
+    color: '#fff',
     // color: "#212121", // по макету
   },
   btn: {
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#FF6C00",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FF6C00',
     height: 50,
     borderRadius: 100,
     marginTop: 43,
   },
   btnTitle: {
     fontSize: 16,
-    color: "#fff",
+    color: '#fff',
   },
 });
 
