3131449b8ad2058bd35121852b5867d9364391e7
K-Basket <koshykkv@gmail.com>
2023-07-22 12:02:59 +0300

    update RegistrationScreen.js

--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -1,38 +1,52 @@
 import {
-  Button,
+  TouchableOpacity,
   ImageBackground,
   StyleSheet,
   Text,
   TextInput,
   View,
+  Platform,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
 import logoBG from "../assets/img/photo-bg.jpg";
 
 export function RegistrationScreen() {
+  console.log(Platform.OS);
+
   return (
-    <View style={styles.container}>
-      <ImageBackground style={styles.imageBG} source={logoBG}>
-        <View style={styles.form}>
-          <View>
-            <Text style={styles.inputTitle}>Login</Text>
-            <TextInput style={styles.input} textAlign="center" />
-          </View>
-          <View style={{ marginTop: 16 }}>
-            <Text style={styles.inputTitle}>E-Mail</Text>
-            <TextInput style={styles.input} textAlign="center" />
-          </View>
-          <View style={{ marginTop: 16 }}>
-            <Text style={styles.inputTitle}>Password</Text>
-            <TextInput
-              style={styles.input}
-              textAlign="center"
-              secureTextEntry={true}
-            />
-          </View>
-          <Button title="Зареєструватися" />
-        </View>
-      </ImageBackground>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <ImageBackground style={styles.imageBG} source={logoBG}>
+          <KeyboardAvoidingView
+            behavior={Platform.OS == "ios" ? "padding" : "height"}
+          >
+            <View style={styles.form}>
+              <View>
+                <Text style={styles.inputTitle}>Login</Text>
+                <TextInput style={styles.input} textAlign="center" />
+              </View>
+              <View style={{ marginTop: 16 }}>
+                <Text style={styles.inputTitle}>E-Mail</Text>
+                <TextInput style={styles.input} textAlign="center" />
+              </View>
+              <View style={{ marginTop: 16 }}>
+                <Text style={styles.inputTitle}>Password</Text>
+                <TextInput
+                  style={styles.input}
+                  textAlign="center"
+                  secureTextEntry={true}
+                />
+              </View>
+              <TouchableOpacity style={styles.btn} activeOpacity={0.8}>
+                <Text style={styles.btnTitle}>Зареєструватися</Text>
+              </TouchableOpacity>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -68,4 +82,23 @@ const styles = StyleSheet.create({
     color: "#fff",
     // color: "#212121", // по макету
   },
+  btn: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FF6C00",
+    height: 50,
+    borderRadius: 100,
+    marginTop: 43,
+  },
+  btnTitle: {
+    fontSize: 16,
+    color: "#fff",
+  },
 });
+
+/*
+
+- KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"} - // fix перекриття клавіатурою форми
+- TouchableWithoutFeedback onPress={Keyboard.dismiss} -- клавіатура знікає при дотику будь де по екрану
+
+*/
