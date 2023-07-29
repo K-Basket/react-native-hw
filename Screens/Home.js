98ae8c47a5337f4647e4ec181796b4238d428f7f
K-Basket <koshykkv@gmail.com>
2023-07-29 15:34:27 +0300

    added Home page

--- Screens/Home.js
+++ Screens/Home.js
@@ -4,17 +4,39 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { CreatePostsScreen } from './CreatePostsScreen';
 import { ProfileScreen } from './ProfileScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useState } from 'react';
+import { RegistrationScreen } from './auth/RegistrationScreen';
+import { LoginScreen } from './auth/LoginScreen';
 
 export function Home() {
+  const [isAuth, setIsAuth] = useState(true);
   const HomeStack = createStackNavigator();
-  const Tab = createBottomTabNavigator; // нижняя навигация
+  const Tabs = createBottomTabNavigator(); // нижняя навигация
 
   return (
-    <HomeStack.Navigator initialRouteName="Posts">
-      <HomeStack.Screen name="Posts" component={PostsScreen} />
-      <HomeStack.Screen name="CreatePosts" component={CreatePostsScreen} />
-      <HomeStack.Screen name="Profile" component={ProfileScreen} />
-    </HomeStack.Navigator>
+    <>
+      {isAuth && (
+        <Tabs.Navigator initialRouteName="Posts">
+          <Tabs.Screen name="Posts" component={PostsScreen} />
+          <Tabs.Screen name="CreatePosts" component={CreatePostsScreen} />
+          <Tabs.Screen name="Profile" component={ProfileScreen} />
+        </Tabs.Navigator>
+      )}
+      {!isAuth && (
+        <HomeStack.Navigator initialRouteName="LoginScreen">
+          <HomeStack.Screen
+            name="Registration"
+            component={RegistrationScreen}
+            options={{ headerShown: false }} // скрывает header
+          />
+          <HomeStack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+        </HomeStack.Navigator>
+      )}
+    </>
   );
 }
 
